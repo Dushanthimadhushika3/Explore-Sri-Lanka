246f5a8b7e5b3a0763af4f1d588f4e54978730ce
dushanthimadhushika3@gmail.com <dushanthimadhushika3@gmail.com>
2019-11-28 00:37:55 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CustomMaterialModule } from './core/material.module';
+import { AppRoutingModule } from './core/app.routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { HomeComponent } from './home/home.component';
+import {MatGridListModule} from "@angular/material/grid-list";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
+    AppRoutingModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/app.routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModule}  from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {UserComponent} from '../user/user.component';
+import {LoginComponent} from '../login/login.component';
+import { HomeComponent } from '../home/home.component';
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent },
+  {path : '', component : HomeComponent}
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
